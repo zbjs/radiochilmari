d174fedd8dc60ec26fb90ae798a560009c2f59a8
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-11-20 16:50:17 +0600

    next build && next export-v2.0.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,37 @@
+
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: process.env.BASE_PATH || '',
-    reactStrictMode: true,
-    images: {
-        domains: ['images.unsplash.com'],
-    },
 
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: {
+      tailwindcss: {},
+    },
     i18n: {
         locales: ['en', 'bn'], 
         defaultLocale: 'en',
       },
-};
+  };
+  
+  export default withNextIntl(config);
 
-export default withNextIntl(nextConfig);
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     basePath: process.env.BASE_PATH || '',
+//     reactStrictMode: true,
+//     images: {
+//         domains: ['images.unsplash.com'],
+//     },
+
+//     i18n: {
+//         locales: ['en', 'bn'], 
+//         defaultLocale: 'en',
+//       },
+// };
+
