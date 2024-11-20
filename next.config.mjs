3c271da3428038eb2f662f3aa5d9b0c6943f275b
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-11-20 16:55:07 +0600

    next build && next export-v2.0.2

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
-
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const nextIntlPlugin = createNextIntlPlugin({
-    locales: ['en', 'bn'],
-    defaultLocale: 'en',
-  });
-
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {},
-      postcss: {},
-      nextIntl: nextIntlPlugin,
-      
       
     },
     
