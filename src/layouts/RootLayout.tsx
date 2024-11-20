ea6fe61f3fc63a2c03ba65fa16bd05f1e87a4cee
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-11-20 18:48:53 +0600

    malti: Layout add this apps

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Welcome to the radiochilmari",
+  description: "radiochilmari",
+  icons: { icon: "/public/apple-touch-icon.png" },
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
