56c2b2de0657370598eead3f2da26805038ab511
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-11-20 16:16:10 +0600

    next build && next export

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: process.env.BASE_PATH || '',
+    reactStrictMode: true,
+    images: {
+        domains: ['images.unsplash.com'],
+    },
+
+    i18n: {
+        locales: ['en', 'bn'], 
+        defaultLocale: 'en',
+      },
+};
 
 export default withNextIntl(nextConfig);
